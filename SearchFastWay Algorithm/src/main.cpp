c96fcfc0b1a8731a62ce20a39c503c5626d37f81
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-09-15 16:31:48 +0300

    Add files via upload

---
+++ SearchFastWay Algorithm/src/main.cpp
@@ -0,0 +1,130 @@
+/*
+     Algorithm is designed for calculated
+     the closest root in 3d coordinates .
+
+     Author : fandimfromitaly@yandex.ru
+     2022 year.
+
+*/
+#include <iostream>
+#include <vector>
+#include "Point.h"
+#include <cmath>
+
+using namespace std;
+double generate_random_number(){
+    double result = 3 + rand() % 300;
+     return result;
+}
+
+Point generate_random_point(){
+
+        double x ,y , z ;
+        int id = 1 + rand() % 452;
+        //get random position
+        x = generate_random_number();
+        y = generate_random_number();
+        z = generate_random_number();
+        //create point
+        Point point;
+        point.set_position(x,y,z);
+        // add point in  list
+        point.id = id;
+        return point;
+
+}
+
+
+
+
+double calculate_root_model(vector<Point> root , vector<Point> points , Point point_A , double fast_distance  ){
+
+
+    //Point point_A; // start point
+    //Point point_C; // end point
+
+
+    //across all list to find fast distance.
+
+    double distance , final_distance;
+    double summ , summ_1 , summ_2 , summ_3;
+
+
+
+    for (int i = 0 ; i != points.size(); i++){
+     cout<<root[i].x_position<<" "<<root[i].y_position<<endl;
+     summ_1 =  points[i].x_position; - point_A.x_position;
+     summ_2 =  points[i].y_position; - point_A.y_position;
+     summ_3 =  points[i].z_position; - point_A.z_position;
+     summ_1 = pow(summ_1,2);
+     summ_2 = pow(summ_2,2);
+     summ_3 = pow(summ_3,2);
+     summ = summ_1+summ_2+summ_3;
+     cout<<"Summ:"<<summ<<endl;
+     cout<<"points:"<<points[i].y_position<<endl;
+     distance = sqrt(summ);
+     cout<<"Distance:"<<distance<<endl;
+
+     distance = sqrt(summ);
+     if(distance < fast_distance){
+
+        fast_distance = distance; // we found closet point.
+
+        cout<<"x:"<<points[i].x_position <<endl;
+
+        root.push_back(points[i]); // add in root .
+        cout<<"x root:"<<root[i].x_position <<endl;
+        // current point is start point now.
+        point_A.x_position = points[i].x_position ;
+        point_A.y_position = points[i].y_position ;
+        point_A.z_position = points[i].z_position ;
+        //remove from points
+        points.erase(points.begin() + points[i].id);
+
+        calculate_root_model(root,points,point_A ,fast_distance );
+        final_distance += distance;
+
+
+     }
+
+    }
+
+
+    return final_distance;
+
+}
+
+
+
+int main()
+{
+
+    cout<<"SearchFastWay"<<endl;
+    vector<Point> root; // points are added in the root
+
+    // create temporal object for searching a closet point.
+
+    double fast_distance = 1000000; // number was taken to initialize search.
+    double final_distance;
+    Point point_A;
+    point_A.set_id(1);
+    point_A.id =1 + rand() % 452324;
+
+    vector<Point> points;
+    // add start point in begin of root.
+    root.push_back(point_A);
+    //generate random amount of points
+
+    for(int i = 0 ; i <= 39 ; i++){
+        points.push_back(generate_random_point());
+
+    }
+
+    //main function
+    final_distance = calculate_root_model(root,points,point_A , fast_distance);
+
+    cout<<"Final Distance:"<<final_distance<<endl;
+
+    system("pause");
+    return 0;
+}
