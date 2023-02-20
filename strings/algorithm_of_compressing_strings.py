7d47b993da8cee78913d95841210cdce5b3bb2c6
Mikhail Shushkovsky <63235817+FANDIM-wolf@users.noreply.github.com>
2023-02-20 17:50:15 +0300

    Create algorithm_of_compressing_strings.py

---
+++ strings/algorithm_of_compressing_strings.py
@@ -0,0 +1,16 @@
+# Input : aaasack . Output : a3s1a1c1k1 
+message = str(input())
+cnt = 1
+x = 1
+j = message[x:x+1]
+for i in message:
+    
+    if i in j:
+        
+        cnt += 1
+   else:
+        print(i, end='')
+        print(cnt, end='')
+        cnt = 1
+    x += 1
+    j = message[x:x+1]
